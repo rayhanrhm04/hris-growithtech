4181fca2fc781aed98f697217ee33a16601817a7
rayhanrhm04 <rayhancb45@gmail.com>
2023-09-04 21:00:51 +0700

    Membuat halaman dashboard eksekutif dan department dev

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+
+Route::get('/dashboard-eksekutif', function () {
+    return view('dashboard-eksekutif');
+});
+
+Route::get('/dashboard-managerdev', function () {
+    return view('departmentdev.dashboard-managerdev');
+});
+
+
