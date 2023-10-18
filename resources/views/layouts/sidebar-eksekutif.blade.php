3b8d95742bf4b0c630d3db591bb8e557047a4e1b
rayhan <haimac@names-MacBook-Pro.local>
2023-10-18 15:24:11 -0700

    memisahkan sidebar eksekutif

--- resources/views/layouts/sidebar-eksekutif.blade.php
+++ resources/views/layouts/sidebar-eksekutif.blade.php
@@ -1,5 +1,4 @@
- <!-- Sidebar -->
- <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
@@ -24,14 +23,19 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item">
-        <a class="nav-link" href="{{ url('/eksekutif/dataPegawai') }}">
-            <i class="fas fa-sharp fa-solid fa-fw fa-users"></i>
-            <span>Data Pegawai</span></a>
-    </li>
-    <li class="nav-item">
         <a class="nav-link" href="{{ url('/eksekutif/kehadiran') }}">
             <i class="fas fa-fw fa-calendar"></i>
             <span>Kehadiran</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ url('/eksekutif/project') }}">
+            <i class="fas fa-fw fa-check"></i>
+            <span>Project</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ url('/eksekutif/dataPegawai') }}">
+            <i class="fas fa-sharp fa-solid fa-fw fa-users"></i>
+            <span>Data Pegawai</span></a>
     </li>
 
     <!-- Nav Item - Pages Collapse Menu -->
@@ -39,22 +43,22 @@
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
             aria-expanded="true" aria-controls="collapseTwo">
             <i class="fas fa-fw fa-file-alt"></i>
-            <span>Pengajuan</span>
+            <span>Pengajuan Izin</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <a class="collapse-item" href="{{ url('/eksekutif/pengajuanCuti') }}">Izin</a>
-                <a class="collapse-item" href="{{ url('/eksekutif/pengajuanReimbursement') }}">Reimbursement</a>
+                <a class="collapse-item" href="{{ url('/eksekutif/pengajuanCuti') }}">Cuti</a>
+                <a class="collapse-item" href="{{ url('/eksekutif/pengajuanReimbursement') }}">Reimbursement Claim</a>
             </div>
         </div>
     </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ url('/eksekutif/project') }}">
-            <i class="fas fa-fw fa-calculator"></i>
-            <span>Penggajian</span></a>
-    </li>
+
     <li class="nav-item">
         <a class="nav-link" href="{{ url('/eksekutif/reporting') }}">
             <i class="fas fa-fw fa-folder-open"></i>
             <span>Reporting</span></a>
     </li>
+
+    <hr class="sidebar-divider">
+
+</ul>
