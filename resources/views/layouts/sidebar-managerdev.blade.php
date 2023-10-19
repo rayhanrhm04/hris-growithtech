8bf5b5ce1ac3058cafd8a1d31f1e9d5747834c93
rayhan <haimac@names-MacBook-Pro.local>
2023-10-18 17:01:45 -0700

    pengajuan reimbursement department dev

--- resources/views/layouts/sidebar-managerdev.blade.php
+++ resources/views/layouts/sidebar-managerdev.blade.php
@@ -66,3 +66,7 @@
     <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
     </div>
+
+    <hr class="sidebar-divider">
+ 
+</ul>
