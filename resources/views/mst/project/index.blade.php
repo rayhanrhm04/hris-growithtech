fe2941649338ffab8353e0775f3aad2285279726
rayhanrhm <rayhancb45@gmail.com>
2023-12-02 18:54:52 -0800

    project

--- resources/views/mst/project/index.blade.php
+++ resources/views/mst/project/index.blade.php
@@ -71,7 +71,7 @@
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                                 Jumlah project</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $jumlahProject }}</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">2</div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-folder fa-2x text-gray-300"></i>
